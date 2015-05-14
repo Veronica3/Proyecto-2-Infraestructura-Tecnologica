3e71ee9d81dc1d0a3098b06662b22e3c79437f4c
andrepao096 <andrepao096@gmail.com>
2015-05-14 14:11:09 -0600

    Paola

--- Proyecto2-ManejoMemoria/src/proyecto2/manejomemoria/Mapa.java
+++ Proyecto2-ManejoMemoria/src/proyecto2/manejomemoria/Mapa.java
@@ -13,6 +13,9 @@ public class Mapa {
         Map<Integer, Proceso> Nombre_Mapa = new HashMap<Integer, Proceso>();/*se crea una estructura mapa con llave valor, 
         la cual almacenara la información correspondiente al disco o a la memoria principal*/
         //kkkk
+        //papñf
+        
+     
     
     }
 }
