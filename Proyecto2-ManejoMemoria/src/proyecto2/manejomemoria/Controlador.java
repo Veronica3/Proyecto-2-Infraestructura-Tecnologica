7d377c4523bfe003e01e30e12641b7ef16dfd6a8
Gabriela <gabyreyes2908@gmail.com>
2015-05-19 08:43:13 -0600

    1) Agregado "int" en DTO para conocer el # de paginas por proceso en memoria principal 2) Creacion de clase controlador

--- Proyecto2-ManejoMemoria/src/proyecto2/manejomemoria/Controlador.java
+++ Proyecto2-ManejoMemoria/src/proyecto2/manejomemoria/Controlador.java
@@ -1,13 +1,14 @@
 
 package proyecto2.manejomemoria;
-
+import java.util.*;
 
 public class Controlador {
     //Constructor de controlador
     public Controlador() {
     }
     //Lee las referencias, llama a la funcion
-    public void Leer_Referencias (){
+    public void Leer_Referencias (LinkedList LReferencias){
+        LinkedList <Interface_Referencia> Lista_Referencias = LReferencias;
         
     }
 }
---
+++ Proyecto2-ManejoMemoria/src/proyecto2/manejomemoria/DTO2.java
@@ -0,0 +1,41 @@
+package proyecto2.manejomemoria;
+import java.util.*;
+public class DTO2 {
+    private static DTO INSTANCE;
+    //Declaracion de variables del DTO
+    LinkedList<Paginas> Lista_Paginas_MM;
+    HashMap<Integer, Marco> Mapa_Marcos = new HashMap<Integer, Marco>();
+    private String Politica_Recuperacion;
+    private String Politica_Ubicacion;
+    private String Politica_Reemplazo;
+    private String Politica_Limpieza;
+    private String Tamaño_Conjunto;
+    private String Ambito_Reemplazo;
+    private String Control_Limpieza;
+    int Paginas_En_MM_Por_Proceso;
+    int Tamaño_Memoria;
+    int Tamaño_Paginas;
+
+    private DTO2() {
+    }
+    
+    //
+    public void setContenidoDTO(String Politica_Recuperacion, String Politica_Ubicacion, String Politica_Reemplazo, String Politica_Limpieza, String Tamaño_Conjunto, String Ambito_Reemplazo, String Control_Limpieza,int Paginas_Por_Proceso, int Tamaño_Memoria, int Tamaño_Paginas) {
+        this.Politica_Recuperacion = Politica_Recuperacion;
+        this.Politica_Ubicacion = Politica_Ubicacion;
+        this.Politica_Reemplazo = Politica_Reemplazo;
+        this.Politica_Limpieza = Politica_Limpieza;
+        this.Tamaño_Conjunto = Tamaño_Conjunto;
+        this.Ambito_Reemplazo = Ambito_Reemplazo;
+        this.Control_Limpieza = Control_Limpieza;
+        this.Tamaño_Memoria = Tamaño_Memoria;
+        this.Tamaño_Paginas = Tamaño_Paginas;
+        this.Paginas_En_MM_Por_Proceso=Paginas_Por_Proceso;
+       }
+       public int TamañoPagina(){
+        return Tamaño_Paginas;
+    }
+    
+    
+    
+}
