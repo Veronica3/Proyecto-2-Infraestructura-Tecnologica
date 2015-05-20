23e0c5a1b32b2e278e8f4fe79ef343a4a41268f3
andrepao096 <andrepao096@gmail.com>
2015-05-20 10:37:57 -0600

    DTO corregido

--- Proyecto2-ManejoMemoria/src/proyecto2/manejomemoria/DTO.java
+++ Proyecto2-ManejoMemoria/src/proyecto2/manejomemoria/DTO.java
@@ -7,8 +7,8 @@ public class DTO {
     LinkedList<Paginas> Lista_Paginas_En_Memoria_Principal= new LinkedList<>();
     LinkedList<Paginas> Paginas_Referenciadas= new LinkedList<>();
     //Memoria FISICA Y VIRTUAL
-    HashMap<Integer, Marco> Memoria_Fisica = new HashMap<Integer, Marco>();
-    LinkedHashMap<String, Paginas> Memoria_Virtual;
+    HashMap<Integer,LinkedHashMap<Integer, Marco>> Memoria_Fisica = new HashMap<Integer,LinkedHashMap<Integer, Marco> >();
+    LinkedHashMap<String, Paginas> Memoria_Virtual=new LinkedHashMap<String, Paginas>();
     //Lista de PROCESOS y REFERENCIAS
     LinkedHashMap<Integer,Interface_Proceso> Lista_Procesos= new LinkedHashMap();
     LinkedList <Interface_Referencia> Lista_Referencias = new LinkedList();
@@ -18,12 +18,18 @@ public class DTO {
     String Politica_Ubicacion;
     String Politica_Reemplazo;
     String Politica_Limpieza;
+    int Tiempo_de_Limpieza; 
     String Tamaño_Conjunto;
+    int Minimo_Marcos;
+    int Maximo_Marcos;
+    int Tamano_aumento;
     String Ambito_Reemplazo;
-    String Control_Limpieza;
-    int Paginas_En_MM_Por_Proceso;
-    int Tamaño_Memoria;
+    int Grado_Multiprogramacion; 
+    int Working_Set;
+    int Tamaño_Memoria_Fisica;
+    int Bits;
     int Tamaño_Paginas;
+    String Unidad_Medida;
     
     //Variables para abrir archivos
     String Nombre_Archivo_Procesos;
@@ -31,20 +37,31 @@ public class DTO {
 
     
     //Constructor de la Clase
-    public DTO(String Politica_Recuperacion, String Politica_Ubicacion, String Politica_Reemplazo, String Politica_Limpieza, String Tamaño_Conjunto, String Ambito_Reemplazo, String Control_Limpieza,int Paginas_Por_Proceso, int Tamaño_Memoria, int Tamaño_Paginas) {
+     
+    
+    public DTO(String Politica_Recuperacion, String Politica_Ubicacion, String Politica_Reemplazo, String Politica_Limpieza, int Tiempo_de_Limpieza, String Tamaño_Conjunto, int Minimo_Marcos, int Maximo_Marcos, int Tamano_aumento, String Ambito_Reemplazo, int Grado_Multiprogramacion, int Working_Set, int Tamaño_Memoria_Fisica, int Bits, int Tamaño_Paginas, String Unidad_Medida, String Nombre_Archivo_Procesos, String Nombre_Archivo_Referencias){
+        
         this.Politica_Recuperacion = Politica_Recuperacion;
         this.Politica_Ubicacion = Politica_Ubicacion;
         this.Politica_Reemplazo = Politica_Reemplazo;
         this.Politica_Limpieza = Politica_Limpieza;
+        this.Tiempo_de_Limpieza = Tiempo_de_Limpieza;
         this.Tamaño_Conjunto = Tamaño_Conjunto;
+        this.Minimo_Marcos = Minimo_Marcos;
+        this.Maximo_Marcos = Maximo_Marcos;
+        this.Tamano_aumento = Tamano_aumento;
         this.Ambito_Reemplazo = Ambito_Reemplazo;
-        this.Control_Limpieza = Control_Limpieza;
-        this.Tamaño_Memoria = Tamaño_Memoria;
+        this.Grado_Multiprogramacion = Grado_Multiprogramacion;
+        this.Working_Set = Working_Set;
+        this.Tamaño_Memoria_Fisica = Tamaño_Memoria_Fisica;
+        this.Bits = Bits;
         this.Tamaño_Paginas = Tamaño_Paginas;
-        this.Paginas_En_MM_Por_Proceso=Paginas_Por_Proceso;
+        this.Unidad_Medida = Unidad_Medida;
+        this.Nombre_Archivo_Procesos = Nombre_Archivo_Procesos;
+        this.Nombre_Archivo_Referencias = Nombre_Archivo_Referencias;
     }
-    
-       public int TamañoPagina(){
+
+    public int TamañoPagina() {
         return Tamaño_Paginas;
     }
     
