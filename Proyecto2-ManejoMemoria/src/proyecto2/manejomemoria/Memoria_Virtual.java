2ec2d3c1bd7eed4b90ad492c335e18a87b7fb33d
Verónica <ver01raq@gmail.com>
2015-05-23 11:16:17 -0600

    Cambio en la creación de memoria virtual

--- Proyecto2-ManejoMemoria/src/proyecto2/manejomemoria/Memoria_Virtual.java
+++ Proyecto2-ManejoMemoria/src/proyecto2/manejomemoria/Memoria_Virtual.java
@@ -2,17 +2,25 @@
 package proyecto2.manejomemoria;
 import java.io.*;
 import java.util.*;
+import java.math.*;
 
 public class Memoria_Virtual {
-    LinkedHashMap<String, Paginas> MemoriaVirtual;
-            
-    public Memoria_Virtual() {
-           MemoriaVirtual= new LinkedHashMap();/*se crea una estructura mapa con llave valor, 
+            DTO Informacion_Configuracion;
+    public Memoria_Virtual(DTO Informacion) {
+        Informacion_Configuracion= Informacion;
+          /*se crea una estructura mapa con llave valor, 
         la cual almacenara la información correspondiente al disco*/
     }
     //Metodo encargado de crear la memoria virtual
-    public LinkedHashMap Crear_Memoria_Virtual(LinkedHashMap<Integer,Interface_Proceso> LProcesos,DTO Configuracion){
-        int cantidad_paginas;
+    public void Crear_Memoria_Virtual(){
+        /*int cantidad_paginas= (Informacion_Configuracion.Bits-Informacion_Configuracion.Tamaño_Paginas)
+        if (Configuracion.Memoria_Virtual.isEmpty()){//pregunta si la memoria virtual esta vacía es el primer proceso que ingresa
+            if (Excede_Tamaño())
+        }
+        else{
+             int Tamaño_Memoria_Virtual_Actual= Configuracion.Tamaño_Paginas*Configuracion.Memoria_Virtual.size();
+        if (Excede_Tamaño()){}
+        LinkedHashMap<Integer,Interface_Proceso> LProcesos= Configuracion.Lista_Procesos;
         Iterator ii= LProcesos.keySet().iterator();//sirve para recorrer el mapa
        while(ii.hasNext()){
            Integer key=(int)ii.next();
@@ -21,23 +29,34 @@ public class Memoria_Virtual {
            LProcesos.get(key).Asignar_Paginas(cantidad_paginas);
            System.out.println("Cantidad paginas: "+cantidad_paginas);
            Crear_Paginas(cantidad_paginas,LProcesos.get(key), Configuracion.Tamaño_Paginas);
-
+       }
            }
-       return MemoriaVirtual;
     }   
+    //Validar tamaño de memoria virtual
+    public boolean Excede_Tamaño_MV(int Tamaño){
+        int tamaño_virtual= Informacion_Configuracion.Bits-//Informacion_Configuracion.Tamaño_Paginas;
+        double Tamaño_Total_Virtual= Math.pow(2,tamaño_virtual);
+        if(Tamaño_Total_Virtual>Tamaño){
+                return false;
+        }
+        else{
+            return true;
+        }
+    }
+    //Crea las paginas que se le asignnaran a cada proceso en memoria virtual
     private void Crear_Paginas(int cantidad_paginas, Interface_Proceso Proceso, int TamañoP){
         Paginas Pagina;
         int ID_Pagina=0;
         int InicioMemoria=0;
-        int FinalMemoria=(TamañoP-1)*1024;
+        int FinalMemoria=TamañoP-1;//unidad base es bits
         for (int i = 0; i < cantidad_paginas; i++) {
             Pagina= new Paginas(ID_Pagina, Proceso.ID_Proceso(),InicioMemoria,FinalMemoria, TamañoP); 
-            MemoriaVirtual.put(Proceso.ID_Proceso()+"_"+ID_Pagina, Pagina);
+            Informacion_Configuracion.Memoria_Virtual.put(Proceso.ID_Proceso()+"_"+ID_Pagina, Pagina);
             ID_Pagina++;
             InicioMemoria=FinalMemoria+1;
-            FinalMemoria=FinalMemoria+(TamañoP*1024);
+            FinalMemoria=FinalMemoria+TamañoP;
             
-        }
+        }*/
 }        
 
 }
