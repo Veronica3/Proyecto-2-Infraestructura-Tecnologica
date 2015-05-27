9216a7287c444acc768aa513bda0583a8f4d5b07
Gabriela <gabyreyes2908@gmail.com>
2015-05-27 11:36:14 -0600

    Implementando Politica de limpieza

--- Proyecto2-ManejoMemoria/src/proyecto2/manejomemoria/Paginas.java
+++ Proyecto2-ManejoMemoria/src/proyecto2/manejomemoria/Paginas.java
@@ -8,10 +8,9 @@ public class Paginas {
     //int InicioMemoria;
     //int FinalMemoria;
     //int Tamaño;
-    //boolean Bit_Presente=false;
+    int Bit_Suciedad=0;
     int Bit_Modificado=0;
-    //Bit_Accedido para Algoritmo de reemplazo MENOS USADO RECIENTEMENTE (LRU)
-    
+   
     public Paginas() {
     }
     
@@ -19,7 +18,6 @@ public class Paginas {
         this.ID_Pagina = ID_Pagina;
         this.ID_Proceso = ID_Proceso;
         //this.Tamaño = Tamaño;
-        //this.Bit_Presente=false;
         //this.InicioMemoria=InicioMemoria;
         //this.FinalMemoria=FinalMemoria;
      }
--- Proyecto2-ManejoMemoria/src/proyecto2/manejomemoria/Politica_Limpieza.java
+++ Proyecto2-ManejoMemoria/src/proyecto2/manejomemoria/Politica_Limpieza.java
@@ -1,6 +1,23 @@
 
 package proyecto2.manejomemoria;
 
+import java.util.LinkedList;
+
 public class Politica_Limpieza {
+    DTO Estructura_DTO;
+    public Politica_Limpieza(DTO Estructura) {
+        Estructura_DTO=Estructura;
+    }
     
+    //Cuando se hace reemplazo
+    public void Limpieza_Por_Demanda(Paginas Pagina_Para_Limpieza){
+        Pagina_Para_Limpieza.Bit_Suciedad=0;
+    }
+    //Cada cierto tiempo
+    public void Pre_Limpieza(){
+        LinkedList<Marco> Memoria_Fisica=Estructura_DTO.Memoria_Fisica;
+        for (int i = 0; i < Memoria_Fisica.size(); i++) {
+           Memoria_Fisica.get(i).Pagina.Bit_Suciedad=0;
+        }
+    }
 }
