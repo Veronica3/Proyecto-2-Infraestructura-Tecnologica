b2bb45f55362265a9e47180a0755165e58d9b1ad
Verónica <ver01raq@gmail.com>
2015-05-28 09:18:40 -0600

    Vista Configuración Lista, revisar el nombre de los datos que se ingresan al DTO

--- Proyecto2-ManejoMemoria/src/proyecto2/manejomemoria/Validaciones.java
+++ Proyecto2-ManejoMemoria/src/proyecto2/manejomemoria/Validaciones.java
@@ -62,27 +62,6 @@ public class Validaciones {
        
    }
    //Metodo que valida que numero sea mayor a cero
-   private boolean Es_mayor_a_Cero(int Numero){
-       if (Numero>0){
-            return true;
-       }
-       else{
-           return false;
-       }
-   }
-   
-   public boolean Datos_Numericos_Vista_Validos(LinkedList<Integer> Elementos){
-       LinkedList <String> Resultado= new LinkedList();
-       String Valor_es_permitido;
-       String Mensaje;
-       
-       for (int i = 0; i < Elementos.size(); i++) {
-           if(!Es_mayor_a_Cero(Elementos.get(i))){
-               return false;
-           }
-       }
-       return true;
-       
-   }
-
 }
+   
+
